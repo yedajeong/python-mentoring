0f0ea71c5018d3e4cb5e945ade2160ed76c1370c
yedajeong <49023751+yedajeong@users.noreply.github.com>
2020-07-21 22:39:43 +0900

    Lec01_C++Basic_2.cpp

---
+++ Lab01/1-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//02 sizeof() : 변수의 byte크기를 return
+int main(){
+	unsigned short siX;
+	unsigned iX;
+	long liX;
+	long long lliX;
+
+	cout << "sizeof(siX):" << sizeof(siX) << endl;
+	cout << "sizeof(iX):" << sizeof(iX) << endl;
+	cout << "sizeof(liX):" << sizeof(liX) << endl;
+	cout << "sizeof(lliX):" << sizeof(lliX) << endl;
+}
