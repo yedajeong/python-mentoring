b8839c947f56e8012b9ba4f0c879df6f7f8afbda
yedajeong <49023751+yedajeong@users.noreply.github.com>
2020-07-21 22:44:13 +0900

    Lec01_C++Basic_4.cpp

---
+++ Lab01/1-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	const double PI = 3.14;
+	char ch1 = 65;
+	char ch2 = ch1 + 32; // a는 ASKII Code 표에서 97
+
+	cout << ch1 << endl;
+	cout << ch2 << endl;
+	cout << PI << endl;
+}
