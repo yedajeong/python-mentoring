a3c14f7f82d14b117ab59e135aa9c49a1872a959
yedajeong <ydj8952@khu.ac.kr>
2020-07-22 21:29:51 +0900

    Lec03&04_Functions_3.cpp

---
+++ Lab03&04/3-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//03
+int main() {
+	double value;
+	value = 5;
+	cout << sqrt(5) << endl;
+	cout << exp(5) << endl;
+	cout << log10(5) << endl; //그냥 log하면 밑이 e
+	cout << cos(5) << endl;
+
+	return 0;
+}
